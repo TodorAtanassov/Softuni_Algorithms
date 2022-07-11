de293439429e9a2c9fdfa4c4865972acfbd1a901
TodorAtanassov <todoratanasov432@gmail.com>
2022-07-11 08:46:10 +0300

    BT

--- Backtracking_pathfinder.py
+++ Backtracking_pathfinder.py
@@ -1,13 +1,13 @@
 def find_all_paths(row, col, lab, direction, path):
-    if row <= 0 or col < 0 or row >= len(lab) or col >= len(lab[0]):
+    if row < 0 or col < 0 or row >= len(lab) or col >= len(lab[0]):
         return
     if lab[row][col] == '*':
         return
-    if lab[row][col] == 'V':
+    if lab[row][col] == 'v':
         return
     path.append(direction)
     if lab[row][col] == 'e':
-        print(' '.join(path))
+        print(''.join(path))
     else:
         lab[row][col] = 'v'
         find_all_paths(row, col + 1, lab, 'R', path)
