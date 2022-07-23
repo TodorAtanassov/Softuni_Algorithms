ba3f03bebc958b230a50753c5edf65711f3e3938
TodorAtanassov <todoratanasov432@gmail.com>
2022-07-23 08:02:39 +0300

    R E

--- Recursion E#4.py
+++ Recursion E#4.py
@@ -3,13 +3,14 @@ class Area:
         self.row = row
         self.col = col
         self.size = size
-       
-        
+
+
 def explore_area(row, col, matrix):
     if row < 0 or col < 0 or row >= len(matrix) or col >= len(matrix[0]):
         return
-    if matrix != '-':
-        return
+    if matrix[row][col] != '-':
+        return 0
+
     matrix[row][col] = 'v'
     result = 1
     result += explore_area(row - 1, col, matrix)
@@ -25,6 +26,7 @@ cols = int(input())
 matrix = []
 for _ in range(rows):
     matrix.append(list(input()))
+
 areas = []
 for row in range(rows):
     for col in range(cols):
@@ -32,3 +34,7 @@ for row in range(rows):
         if size == 0:
             continue
         areas.append(Area(row, col, size))
+
+print(f'Total areas found : {len(areas)}')
+for idx, area in enumerate(areas):
+    print(f'Area #{idx + 1} at ({area.row}, {area.col}), size : {area.size}')
