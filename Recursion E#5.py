abccec6ebfb11d21646985f9c40a29a9193803b3
TodorAtanassov <todoratanasov432@gmail.com>
2022-07-24 09:41:34 +0300

    R E

---
+++ Recursion E#5.py
@@ -0,0 +1,26 @@
+words = input().split(', ')
+target = input()
+
+words_by_idx = {}
+words_count = {}
+
+for word in words:
+    if word in words_count:
+        words_count[word] += 1
+    else:
+        words_count[word] = 1
+
+    try:
+        idx = 0
+        while True:
+            idx = target.index(word, idx)
+
+        if idx not in words_by_idx:
+            words_by_idx[idx] = []
+            words_by_idx[idx].append(word)
+            idx += len(word)
+    except ValueError:
+        pass
+
+print(words_by_idx)
+print(words_count)
